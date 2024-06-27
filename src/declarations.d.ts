ba773b4b9dda8a0fc34f497bed478bad201a0179
Pavel Bartos <pbartos03@mail.ru>
2024-06-27 19:06:13 +0300

    add declarate file

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg' {
+  import { FC } from 'react';
+  const SVG: FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
